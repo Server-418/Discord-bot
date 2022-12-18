f145e227f3719a0b8baaadd3670edd2c721b5660
0x1AE7F <julian.denner@protonmail.com>
2022-12-18 18:40:57 +0100

    Sandboxing, switch statement for commands

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const discord = require("discord.js")
-const { Client,Collection,Events, GatewayIntentBits,ActivityType, Message, messageLink, TextChannel, Discord,EmbedBuilder} = require('discord.js');
+const sandbox = require("sandbox")
+const { Client,Collection,Events, GatewayIntentBits,ActivityType, Message, messageLink, TextChannel, Discord, EmbedBuilder} = require('discord.js');
 
 
 const { prefix ,token } = require('./config.json');
+const Sandbox = require("sandbox");
 
 
 const client = new Client({ intents: [
@@ -25,15 +27,26 @@ client.once('ready', () => {
 client.on('messageCreate',async (Message) => {
 	if(Message.author.bot) {return}
   if(!Message.content.startsWith(prefix)){return}
-  const cnt = Message.content.toLowerCase();
-  const cmd = cnt.slice(1);//to remove the prefix
+  const cmd = Message.content.slice(1).split(" ")[0].toLowerCase(); //to remove the prefix and arguments
+  const args = Message.content.slice(cmd.length+prefix.length).trim()
 
-
-  if(cmd === "ping"){
-    Message.channel.send("pong");
-}
-
+  switch (cmd) {
+    case "ping": Message.channel.send("pong"); break;
+    case "exec": compileCode(args, Message); break;
+    default: Message.channel.send("[418] I'm a Teapot!"); break;
+  }
 });
 
+
+function compileCode(code, Message) {
+  const regexPattern = new RegExp(/`{1,}(.*?)`{1,}/); // Snippet that is wrapped in atleast one backtick ( Discord code block )
+  var match = regexPattern.exec(code.replace(/\n/g, " "));
+  if (match.length < 1) {Message.channel.send("Please wrap your code in backticks! (```) ")}
+  // TODO: Add support for multiple code snippets
+  var s = new Sandbox();
+  s.run(match[1], function(output) {
+  Message.channel.send("result:\n`"+output.result+"`\nconsole:\n[`"+output.console.join(",")+"`]");
+  });
+}
 
 client.login(token);
