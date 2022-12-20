1955b3ab50df43cd0c0c8b1edc697b0af626074d
SneakyNieky <niekdommelen37@gmail.com>
2022-12-20 10:35:38 +0100

    added MessageCreate.js. it now contains all the command this will later be replaced

---
+++ events/MessageCreate.js
@@ -0,0 +1,22 @@
+const { Events} = require('discord.js');
+
+module.exports = {
+  name: Events.MessageCreate,
+    once: false,
+	execute(Message) {
+        
+        const {prefix} = require('../config.json');
+        
+        if(Message.author.bot) {return;}
+        if(!Message.content.startsWith(prefix)){return;}
+        const cmd = Message.content.slice(1).split(" ")[0].toLowerCase(); //to remove the prefix and arguments
+        const args = Message.content.slice(cmd.length+prefix.length).trim()
+        
+        switch (cmd) {
+          case "ping": Message.channel.send("pong"); console.log("pong!"); break;
+          case "exec": compileCode(args, Message); break;
+          default: Message.channel.send("[418] I'm a Teapot!"); break;
+        }
+      
+ },
+};
